79b3471949bd84b5bc06bcbd8b3d3e7388bbc43c
GarlicGladiator <zhoujason2003@gmail.com>
2025-12-01 23:51:40 -0500

    terrible blockout lol

--- Assets/Scripts/Entities/Player.cs
+++ Assets/Scripts/Entities/Player.cs
@@ -4,10 +4,6 @@ public class Player : EntityBase
 {
     public static Player instance {get; private set;}
 
-    [Header("FMOD events")]
-    public string FMODDeathEvent = "event:/DeathEvent";
-    public FMOD.Studio.EventInstance playerDeath;
-
     protected override void Init()
     {
         if(instance == null){ instance = this; }
@@ -17,22 +13,13 @@ public class Player : EntityBase
         entityName = "The Yellow Runner";
         base.Init();
 
-
-        playerDeath = FMODUnity.RuntimeManager.CreateInstance(FMODDeathEvent);
-    }
-
-    void Update()
-    {
-        FMODUnity.RuntimeManager.AttachInstanceToGameObject(playerDeath, transform);
     }
 
     protected override void OnDeath()
     {
         // Put all your death event code here BEFORE base.Death()
-        playerDeath.start();
-
-
-
+        entityDeath.start();
+        
         // Will Handle the destruction of the object. 
         // If you dont want the object to dissappear or want to "delay" its true death for an animation or smth, consider Invoke() or coroutines to delay it
         // Or call it somewhere else and delete this reference.
