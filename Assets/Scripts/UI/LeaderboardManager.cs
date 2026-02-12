63264fa45f6ba2a2d4c2298184a8db6c40bc1bc2
ELITEGameStudios <100547900+ELITEGameStudios@users.noreply.github.com>
2026-02-12 05:50:43 -0500

    Remove Visual Scripting References

--- Assets/Scripts/UI/LeaderboardManager.cs
+++ Assets/Scripts/UI/LeaderboardManager.cs
@@ -3,7 +3,6 @@ using System;
 using UnityEngine;
 using System.Collections.Generic;
 using System.Linq;
-using Unity.VisualScripting;
 using System.IO;
 using TMPro;
 using UnityEngine.UI;
--- Assets/Scripts/UI/SettingsMenu.cs
+++ Assets/Scripts/UI/SettingsMenu.cs
@@ -3,7 +3,6 @@ using System.Collections;
 using System.Globalization;
 using TMPro;
 using Unity.Mathematics;
-using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.Rendering;
