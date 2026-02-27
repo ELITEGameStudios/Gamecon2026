081f7fb6ad6fc95a1c17aff236cc960b194fa4ad
ELITEGameStudios <100547900+ELITEGameStudios@users.noreply.github.com>
2026-02-27 15:33:13 -0500

    no longer grounded in triggers

--- Assets/Scripts/UI/GroundedHelper.cs
+++ Assets/Scripts/UI/GroundedHelper.cs
@@ -15,6 +15,8 @@ public class GroundedHelper : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void OnTriggerEnter(Collider other)
     {
+        if(other.isTrigger) return;
+        
         colliders.Add(other);
         isGrounded = true;
 
