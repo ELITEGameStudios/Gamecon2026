44039d87cb1e458cd69b224a2f445667b8aa1af5
Eman Awo <emanawosi4@gmail.com>
2026-04-02 05:14:17 -0400

    Parry Wing VFX
    Added new wings VFX to Parry
    
    Material is a placeholder for now

---
+++ Assets/Scripts/VFX/ParryWingsManager.cs
@@ -0,0 +1,54 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParryWingsManager : MonoBehaviour
+{
+    [SerializeField] Animator leftWingsAnimator;
+    [SerializeField] Animator rightWingsAnimator;
+    [SerializeField] Projectile knife;
+
+    [SerializeField] Transform leftWingsTransform;
+    [SerializeField] Transform rightWingsTransform;
+
+    List<SkinnedMeshRenderer> skinnedMeshes = new();
+    List<MeshRenderer> regularMeshes = new();
+    private void Start()
+    {
+        knife.projectileAbilities.attemptedParry.AddListener(OnParryAttempted);
+        knife.stateChanged.AddListener(OnParryEnded);
+        var leftWingRenderers = leftWingsTransform.GetComponentsInChildren<SkinnedMeshRenderer>();
+        var rightWingRenderers = rightWingsTransform.GetComponentsInChildren<SkinnedMeshRenderer>();
+        foreach (var renderer in leftWingRenderers) skinnedMeshes.Add(renderer);
+        foreach (var renderer in rightWingRenderers) skinnedMeshes.Add(renderer);
+
+        var leftWingRegularRenderers = leftWingsTransform.GetComponentsInChildren<MeshRenderer>();
+        var rightWingRegularRenderers = rightWingsTransform.GetComponentsInChildren<MeshRenderer>();
+        foreach (var renderer in leftWingRegularRenderers) regularMeshes.Add(renderer);
+        foreach (var renderer in rightWingRegularRenderers) regularMeshes.Add(renderer);
+        OnParryEnded(Projectile.ProjectileState.Idle);
+    }
+
+    void OnParryAttempted(bool successful)
+    {
+        if (!successful) return;
+
+        leftWingsAnimator.SetTrigger("ParryPerformed");
+        rightWingsAnimator.SetTrigger("ParryPerformed");
+        // point wings in the direction the knife is flying
+        foreach (var renderer in skinnedMeshes) renderer.enabled = true;
+        foreach (var renderer in regularMeshes) renderer.enabled = true;
+
+    }
+
+    void OnParryEnded(Projectile.ProjectileState state)
+    {
+        if (state == Projectile.ProjectileState.Flying) return; // means it was just fired by the parry, ignore it
+        foreach (var renderer in skinnedMeshes) renderer.enabled = false;
+        foreach (var renderer in regularMeshes) renderer.enabled = false;
+    }
+
+
+
+
+
+}
