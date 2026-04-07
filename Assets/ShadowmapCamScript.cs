cc599d4999a5bf8694da3cb9e4c1f5431e65a48c
ELITEGameStudios <100547900+ELITEGameStudios@users.noreply.github.com>
2026-04-07 08:34:10 -0400

    pre build

--- Assets/ShadowmapCamScript.cs
+++ Assets/ShadowmapCamScript.cs
@@ -1,18 +1,24 @@
 using UnityEngine;
 
-[ExecuteAlways]
+// [ExecuteAlways]
 public class ShadowmapCamScript : MonoBehaviour
 {
     public Camera cam;
     [SerializeField] private Matrix4x4 latestMatrix;
     [SerializeField] private Material[] materials;
     [SerializeField] private RenderTexture texture;
+    [SerializeField] private int activeFrames;
 
     public static ShadowmapCamScript instance {get; private set;}
     void Awake()
     {
         if(instance == null){instance = this;}
         else if(instance != this){Destroy(this);}
+    }
+
+    void Start()
+    {
+        activeFrames = 5;
     }
 
     public void SetTexture(RenderTexture texture)
@@ -55,6 +61,10 @@ public class ShadowmapCamScript : MonoBehaviour
                     material.SetVector("_ShadowmapCamPos", cam.transform.position);
                 }
             }
+            if(activeFrames <= 0){gameObject.SetActive(false); return;}
+            activeFrames--;
+
+
             
         // }
     }
