0d3ee68506f38bb790bd92f944a0aab00814613e
Eman Awo <emanawosi4@gmail.com>
2026-04-09 07:35:10 -0400

    Knife Restoration On Bounds Collision
    Knife is now restored when either
    
    A: Killbox is a limiter (cannot go out otherwise you die) and the knife exits
    B: killbox is a hazard (cannot go in otherwise you die) and the knife enters
    
    knife interactions with the killbox can be toggled

--- Assets/Scripts/LevelTools/Hazards/Killbox.cs
+++ Assets/Scripts/LevelTools/Hazards/Killbox.cs
@@ -6,31 +6,32 @@ public class Killbox : MonoBehaviour
     /// If this is true, destroys entities that exit the trigger area. Prevents player from being able to escape play area.
     /// </summary>
     [SerializeField] bool isPlayAreaLimiter = false;
+    [SerializeField] bool affectKnife = true;
+    Collider killboxCollider;
+
+    LayerMask knifeMask;
+
+    Collider[] knifeHolder;
+
+    private void Start()
+    {
+        killboxCollider = GetComponent<Collider>();
+        knifeMask = LayerMask.GetMask("Projectile");
+    }
     private void OnTriggerEnter(Collider other)
     {
         if (isPlayAreaLimiter) return;
-        if (other.TryGetComponent(out EntityBase entity))
-        {
-            entity.Die();
-        }
-        else if (other.TryGetComponent(out EnemyProjectile proj))
-        {
-            foreach (var mod in proj.projectileModifiers)
-            {
-                if (mod.GetType() == typeof(ProjectileIndestructibleModifier))
-                {
-                    return;
-                }
-            }
-            proj.DestroyProjectile();
-        }
+        ClearEntity(other);
     }
 
     private void OnTriggerExit(Collider other)
     {
         if (!isPlayAreaLimiter) return;
+        ClearEntity(other);
+    }
 
-
+    void ClearEntity(Collider other)
+    {
         if (other.TryGetComponent(out EntityBase entity))
         {
             entity.Die();
@@ -45,6 +46,16 @@ public class Killbox : MonoBehaviour
                 }
             }
             proj.DestroyProjectile();
+        }
+        else if (other.TryGetComponent(out Projectile knife))
+        {
+            // if the killbox kills things inside of the box, and the knife is within the box, give it back to the player
+            //if the killbox kills things outside of the box, and the knife is outside the box, give it back to the player
+
+            if (!affectKnife) return;
+            bool insideContainer = killboxCollider.bounds.Contains(knife.transform.position);
+           
+            if (insideContainer && !isPlayAreaLimiter || !insideContainer && isPlayAreaLimiter) knife.Pickup();
         }
     }
 
